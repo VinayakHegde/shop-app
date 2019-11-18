ecb05ecea9375340b068b914750d5def39136368
Vinayak Hegde <vinayak.nandi@gmail.com>
2019-11-18 21:06:27 +0000

    Updates

---
+++ precache-manifest.12136a0e23561c13a72ca7c1d5217048.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a26a4a64f3edb05915b4c7d321012a3a",
+    "url": "/shop-app/index.html"
+  },
+  {
+    "revision": "b578e42ea2f900957bf2",
+    "url": "/shop-app/static/css/main.ab3506bb.chunk.css"
+  },
+  {
+    "revision": "55a1cdae84ccd12deb6e",
+    "url": "/shop-app/static/js/2.d9a7e05e.chunk.js"
+  },
+  {
+    "revision": "b578e42ea2f900957bf2",
+    "url": "/shop-app/static/js/main.df5234ae.chunk.js"
+  },
+  {
+    "revision": "527f0adb22613502ab93",
+    "url": "/shop-app/static/js/runtime-main.5faaf4e9.js"
+  }
+]);
